f27faceadd43ddfc32dbb7c6f4d098a217afa682
Siddharth Kumar <ybsid@Siddharths-MacBook-Pro.local>
2023-07-23 16:10:41 +0530

    solutions and test for Leap and space-age

--- src/main/scala/com/ybsid/exercism/easy/Leap.scala
+++ src/main/scala/com/ybsid/exercism/easy/Leap.scala
@@ -1,5 +1,13 @@
 package com.ybsid.exercism.easy
 
-object Leap {
 
+/**
+ * https://exercism.org/tracks/scala/exercises/leap
+ */
+object Leap {
+  def leapYear(year: Int): Boolean = {
+    val mod4 = year%4==0
+    val rem  = year%100!=0 ||  year%400 == 0
+    mod4 &&  rem
+  }
 }
--- src/main/scala/com/ybsid/exercism/easy/SpaceAge.scala
+++ src/main/scala/com/ybsid/exercism/easy/SpaceAge.scala
@@ -1,5 +1,42 @@
 package com.ybsid.exercism.easy
 
+/**
+ * https://exercism.org/tracks/scala/exercises/space-age
+ */
 object SpaceAge {
-
+  private val toYearSeconds = 31556952
+  def onEarth(age: Double): Double = {
+    val period = 1.0
+    (age/toYearSeconds) / period
+  }
+  def onVenus(age: Double): Double = {
+    val period = 0.61519726
+    onEarth(age) / period
+  }
+  def onMercury(age: Double): Double = {
+    val period = 0.2408467
+    onEarth(age) / period
+  }
+  def onMars(age: Double): Double = {
+    val period = 1.8808185
+    onEarth(age) / period
+  }
+  def onJupiter(age: Double): Double = {
+    val period = 11.862615
+    onEarth(age) / period
+  }
+  def onSaturn(age: Double): Double = {
+    val period = 29.447498
+    onEarth(age) / period
+  }
+  def onUranus(age: Double): Double = {
+    val period = 84.016846
+    onEarth(age) / period
+  }
+  def onNeptune(age: Double): Double = {
+    val period = 164.79132
+    onEarth(age) / period
+  }
 }
+
+
